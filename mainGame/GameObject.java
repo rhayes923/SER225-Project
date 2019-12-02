ffff3b978a39146ad62dbc753e73fed960e2317b
Ryan Hayes <ryanhayes923@gmail.com>
2019-12-02 12:27:25 -0500

    merge with master

--- mainGame/GameObject.java
+++ mainGame/GameObject.java
@@ -30,7 +30,7 @@ public abstract class GameObject {
 	public abstract void tick();
 
 	public abstract void render(Graphics g);
-
+	
 	public abstract Rectangle getBounds();
 
 	// non abstract can be accessed via OBJECT.________, does not need to be
